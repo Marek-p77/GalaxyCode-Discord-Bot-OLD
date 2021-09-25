a99c1d52f32231fc6141e2ef01ac798ba696b2a7
Marek_p <marek.mnp@seznam.cz>
2021-09-25 22:52:33 +0200

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: "status",
-    description: "Status minecraft serveru",
+    name: "status", // Jméno commandu
+    description: "Status minecraft serveru", // Popis commandu
     execute(Discord, client, message) {
-        console.log("status");
+        console.log("[LOG] Status command proveden"); // Console log o provedení status commandu 
         const axios = require('axios')
         lastEmbedChannelId = message.channel.id
 
@@ -11,32 +11,32 @@ module.exports = {
             .split(" ")
         if (args[0]) {
             axios
-                .get("https://api.mcsrvstat.us/2/" + args[0])
+                .get("https://api.mcsrvstat.us/2/" + args[0]) // API Endpoint
                 .then(res => {
-                    const embed = new Discord.MessageEmbed()
-                        .setTitle(args[0] + " Status")
+                    const embed = new Discord.MessageEmbed() // Nový embed
+                        .setTitle(args[0] + " Status") // Nadpis embedu
                     if (res.data.online && (res.data.debug.ping || res.data.debug.query || res.data.debug.srv) && !args[0].includes("qplay")) {
-                        embed.setColor("#000")
-                        embed.addField("Status", "Online", true)
+                        embed.setColor("#000") // Barva online embedu
+                        embed.addField("Status", "Online", true) // Status online
                         if (res.data.players.online) {
-                            embed.addField("Počet hráčů", res.data.players.online + "/" + res.data.players.max, true)
+                            embed.addField("Počet hráčů", res.data.players.online + "/" + res.data.players.max, true) // Počet hráčů
                             if (res.data.debug.query) {
                                 let playersText = ""
                                 res.data.players.list.forEach(player => {
                                     playersText += "`" + player + "` "
                                 })
-                                embed.addField("Hráči", playersText)
+                                embed.addField("Hráči", playersText) // Pole hráči
                             }
                         } else {
                             embed.addField("Počet hráčů", "0/0", true)
                         }
-                        embed.addField("Verze", res.data.version, true)
-                        embed.addField("IP Adresa", args[0])
-                        embed.addField("MOTD", res.data.motd.clean[0] + "\n" + res.data.motd.clean[1])
-                        embed.addField("Host", res.data.hostname, true) + "\n" +
-                        embed.addField("IP", res.data.ip, true)
-                        embed.addField("Port", res.data.port, true)
-                        embed.addField("Software", res.data.software, true)
+                        embed.addField("Verze", res.data.version, true) // Verze serveru
+                        embed.addField("IP Adresa", args[0]) // IP Adresa serveru
+                        embed.addField("MOTD", res.data.motd.clean[0] + "\n" + res.data.motd.clean[1]) // MOTD serveru
+                        embed.addField("Host", res.data.hostname, true) + "\n" + // Hostname serveru
+                        embed.addField("IP", res.data.ip, true) // IP Adresa serveru
+                        embed.addField("Port", res.data.port, true) // Port serveru
+                        embed.addField("Software", res.data.software, true) // Software serveru
 
                         if (res.data.info) {
                             let infoText = ""
@@ -46,30 +46,30 @@ module.exports = {
                             embed.addField("Info", infoText)
                         }
 
-                        embed.setFooter("", "")
+                        embed.setFooter("", "") // Prázdný footer pro název vašeho serveru :)
 
                         embed.setTimestamp(new Date)
                     } else if ((res.data.debug.ping || res.data.debug.query || res.data.debug.sr) && !args[0].includes("qplay")) {
-                        embed.setColor("#000")
-                        embed.addField("Status", "offline", true)
-                        embed.addField("Počet Hráčů", "0/0", true)
-                        embed.addField("Verze", "-", true)
+                        embed.setColor("#000") // Barva offline embedu
+                        embed.addField("Status", "offline", true) // Offline status
+                        embed.addField("Počet Hráčů", "0/0", true) // Offline hráči
+                        embed.addField("Verze", "-", true) // Offline verze
 
-                        embed.setFooter("", "")
+                        embed.setFooter("", "") // Prázdný footer pro název vašeho serveru :)
 
-                        embed.setTimestamp(new Date)
+                        embed.setTimestamp(new Date) // Čas ve footeru
                     } else {
-                        embed.setColor("#DF484A")
+                        embed.setColor("#DF484A") // Error embed barva + níže zpráva
                         embed.addField("<:x:869253018225373205> Error", "Server nenalezen. Buď jste špatně napsali adresu, nebo má server zakázáno zjišťovat status.")
                     }
                     client.channels.cache.get(lastEmbedChannelId).send(embed)
                 })
                 .catch(error => {
-                    console.error(error)
+                    console.error(error) // Console log error
                 })
 
         } else {
-            message.channel.send("<:x:869253018225373205> Správné použití: **status <adresa>**")
+            message.channel.send("<:x:869253018225373205> Správné použití: **status <adresa>**") // Správné použití (odpověď na command "status")
         }
     }
 }
